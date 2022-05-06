3635ab23d3c4d06f958b229dbbe7b5495f197870
Antonio Eloy <101589161+antonioaraujozup@users.noreply.github.com>
2022-05-06 11:13:45 -0300

    Commit inicial

---
+++ src/main/java/br/com/zup/edu/bookflyway/BookFlywayApplication.java
@@ -0,0 +1,13 @@
+package br.com.zup.edu.bookflyway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookFlywayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookFlywayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/zup/edu/bookflyway/BookFlywayApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zup.edu.bookflyway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookFlywayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
